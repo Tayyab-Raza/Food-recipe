86ac44c60fa41a6bd03495c2e2bd4b71a97a338a
TayyabRaza <159689011+Tayyab-Raza@users.noreply.github.com>
2024-09-27 22:34:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin');
   res.header('Access-Control-Allow-Methods');
   res.header('Access-Control-Allow-Headers');
-  res.header('Access-Control-Allow-Credentials');
   if (req.method === 'OPTIONS') {
     return res.status(200).end(); // Send a 200 response for OPTIONS
   }
