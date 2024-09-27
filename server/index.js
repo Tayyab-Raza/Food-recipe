6b94c81c58ac3bc41863854871e69adb7943e30d
TayyabRaza <159689011+Tayyab-Raza@users.noreply.github.com>
2024-09-27 22:18:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,10 +16,10 @@ app.use(cors(corsOptions));
 
 //Handle OPTIONS Request Manually 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://reciperiver.netlify.app/');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin');
+  res.header('Access-Control-Allow-Methods');
+  res.header('Access-Control-Allow-Headers');
+  res.header('Access-Control-Allow-Credentials');
   if (req.method === 'OPTIONS') {
     return res.status(200).end(); // Send a 200 response for OPTIONS
   }
