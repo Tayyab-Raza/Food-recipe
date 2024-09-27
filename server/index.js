98dfe44dd65683c99f576ae8f04c23f48dbf692d
TayyabRaza <159689011+Tayyab-Raza@users.noreply.github.com>
2024-09-27 18:05:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const { mongoose } = require("mongoose");
 const app = express();
-const cookieParser = require('cookie-parser')
-const app = express();
+const cookieParser = require('cookie-parser');
 
 //database connection
 mongoose.connect(process.env.MONGO_URL)
