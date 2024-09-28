1eb16add9acb7f10d22a5db1475926db25a6e71e
TayyabRaza <159689011+Tayyab-Raza@users.noreply.github.com>
2024-09-28 10:09:59 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -11,6 +11,13 @@ const {
   recipeGen
 } = require("../controllers/authController");
 
+router.use(cors({
+  origin: 'https://reciperiver.netlify.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow specific HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Allow specific headers
+  credentials: true
+}));
+
 router.get("/", test);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
