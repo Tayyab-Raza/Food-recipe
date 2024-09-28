6581aaf8da0cd2e0e9c8e3a789971c7984abe1fb
TayyabRaza <159689011+Tayyab-Raza@users.noreply.github.com>
2024-09-28 10:11:16 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -18,6 +18,17 @@ router.use(cors({
   credentials: true
 }));
 
+//Handle OPTIONS Request Manually 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin');
+  res.header('Access-Control-Allow-Methods');
+  res.header('Access-Control-Allow-Headers');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // Send a 200 response for OPTIONS
+  }
+  next();
+});
+
 router.get("/", test);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
